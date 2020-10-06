98f3678f4e7acba6e36cb19a5ed7379520a3145b
Hlebushek S Maslom <43886961+Hlebushek-S-Maslom@users.noreply.github.com>
2020-10-06 21:40:55 +0500

    Continued yo change slider

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,19 @@ $(function()
 			{
 				nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
 				prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
-				infinite: false
+				rows: 1,
+				slidesPerRow: 1,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				// speed: 600,
+				// autoplay: true,
+				// autoplaySpeed: 5000,
+				arrows: false,
+				dots: true,
+				swipe: true,
+				swipeToSlide: true,
+				pauseOnHover: true,
+				pauseOnDotsHover: true,
+				infinite: false,
 			})
 	});
